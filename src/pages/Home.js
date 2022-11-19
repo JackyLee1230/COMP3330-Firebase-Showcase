26eb7bc862c74593ac0e151b06f5e4a812801327
Lee Chi Ho <u3578552@connect.hku.hk>
2022-11-20 00:26:20 +0800

    fix: redirect on login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Home = () => {
+	const { user } = UserAuth();
+	if (user) {
+		return <Navigate to="/account" />;
+	}
 	return (
 		<div>
 			<h1>Home Page</h1>
